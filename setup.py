8b463674963b11033c5d5473848e0e5ba4a69d41
nathan.derave <nathan.derave@dataminded.be>
2021-07-21 11:44:51 +0200

    Added setup.cfg

--- setup.py
+++ setup.py
@@ -10,20 +10,5 @@ with open('requirements.txt') as f:
         requirements.append(str(req).replace('==', '>='))
 
 setup(
-    name="salutations-maker", # Replace with your own username
-    version="0.0.1",
-    author="Nathan Derave",
-    author_email="nathan.derave@dataminded.be",
-    description="A package to say hello",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    python_requires='>=3.6',
-    packages=['slmaker'],
-    package_dir={'': 'src'},
-    install_requires=requirements,
-    extras_require={
-        'dev': ['pytest', 'flake8', 'tox'],
-    }
-    
-)
+    install_requires=requirements)
