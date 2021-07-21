97d7aba5ee1de36710fc4c83856fe416df223c8e
nathan.derave <nathan.derave@dataminded.be>
2021-07-21 11:21:16 +0200

    Added type hints and doctstring

--- src/greetings.py
+++ src/greetings.py
@@ -1,5 +1,13 @@
-def say_hello(name, language):
-    
+def say_hello(name: str, language: str) -> None:
+    """A simple helper to say hello
+
+    Args:
+        name (str): The name to greet
+        language (str): The language to greet name with
+
+    Raises:
+        Exception: Exeception raised if the language isn't implemented yet
+    """
     if language=="FR":
         print(f"Bonjour {name}")
 
